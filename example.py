f0fd27e157c2b4b20a33f19c6d6dccaa3a0735f3
bonddd <bonddd@gmail.com>
2023-05-05 23:44:11 +0300

    2 files modified (base_page.ry, example.py)

--- example.py
+++ example.py
@@ -1,17 +1,14 @@
-import time
+from pages.main_page import *
 
-from pages.locators import MainPage
-from pages.base_page import BasePage
 
-base_page = BasePage()
-base_page.open("https://ya.ru")
-time.sleep(5)
+class TestMainPage:
 
-search_input = base_page.find_element(MainPage.SEARCH_INPUT)
-search_input.click()
-search_input.send_keys("почта")
+    def test_visible_of_elements(self, driver):
+        main_page = MainPage(driver, "https://ya.ru")
+        main_page.open()
+        main_page.elements_are_present()
+        # main_page.type_search_request()
+        # main_page.click_button()
 
-search_button = base_page.find_element(MainPage.SEARCH_BUTTON)
-search_button.click()
-time.sleep(5)
+
 
--- pages/base_page.py
+++ pages/base_page.py
@@ -5,34 +5,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 
 class BasePage:
 
-    def open(self, url):
-        driver.get(url)
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
 
-    def find_element(self, locator, time=10):
-        wait = WebDriverWait(driver, time)
-        return wait.until(EC.presence_of_element_located(locator), message=f"Can't find element by locator {locator}")
+    def open(self):
+        self.driver.get(self.url)
+
+    def element_is_present(self, locator, time=20):
+        wait = WebDriverWait(self.driver, time)
+        return wait.until(EC.presence_of_element_located(locator),
+                          message=f"Can't find element by locator {locator}")
 
     def find_elements(self, by, value):
         return self.find_elements(by, value)
 
     def page_title(self):
-        return driver.title
-
-    def is_element_present(self, how, what):
-        try:
-            driver.find_element(how, what)
-        except NoSuchElementException:
-            return False
-        return True
+        return self.driver.title
 
     def page_reload(self):
-        driver.refresh()
+        self.driver.refresh()
 
-    def find_element_with_js(self, value):
-        return driver.execute_script(value)
+    def execute_script(self, value):
+        return self.driver.execute_script(value)
 
