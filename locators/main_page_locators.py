c0e4e407fd292388613a842b9d858eaf80216ec2
bonddd <bonddd@gmail.com>
2023-05-05 23:45:12 +0300

    1 file modified (main_page_locators.py)

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPage:
+class MainPageLocators:
     LOGO = (By.CSS_SELECTOR, 'a[aria-label="Яндекс"]')
     BURGER_BUTTON = (By.CSS_SELECTOR, ".headline__personal-icon>svg")
     ENTER_BUTTON = (By.CSS_SELECTOR, '[data-statlog="headline.enter"]')
@@ -11,8 +11,8 @@ class MainPage:
     CAMERA_BUTTON = (By.CSS_SELECTOR, '.search3__svg_camera')
     CLEAR_BUTTON = (By.CSS_SELECTOR, 'button[title="Очистить поисковый запрос"]')
     WEATHER_ICON = (By.CSS_SELECTOR, "div.informers3__item-icon")
-    USD_MOEX = (By.CSS_SELECTOR, 'a[title="USD MOEX"]')
-    EUR_MOEX = (By.CSS_SELECTOR, 'a[title="EUR MOEX"]')
+    USD_MOEX = (By.CSS_SELECTOR, '[title="USD MOEX"]>span')
+    EUR_MOEX = (By.CSS_SELECTOR, '[title="EUR MOEX"]>span')
     GEO_HOME = (By.CSS_SELECTOR, '[data-statlog="informers.geo"]')
     ALICE_ICON = (By.TAG_NAME, "circle")
 
