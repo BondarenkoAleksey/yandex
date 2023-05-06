f371815cdc63a0e180f059608e18f3dc7a2a088a
bonddd <bonddd@gmail.com>
2023-05-06 23:13:15 +0300

    1 file added

---
+++ tests/test_main_page.py
@@ -0,0 +1,17 @@
+import time
+
+from pages.main_page import *
+
+
+class TestMainPage:
+
+    def test_visible_of_elements_on_main_page(self, driver):
+        main_page = MainPage(driver, "https://ya.ru")
+        main_page.open()
+        main_page.elements_are_present()
+        main_page.type_search_request()
+        main_page.elements_are_present_after_type_search_word()
+        time.sleep(5)
+
+
+
