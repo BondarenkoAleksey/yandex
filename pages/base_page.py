1f5ce1d29fe4c8fa1f62db1c7ee0f8a8d3fce98d
bonddd <bonddd@gmail.com>
2023-05-09 23:08:08 +0300

    1 file modified base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -11,10 +12,13 @@ class BasePage:
     def open(self):
         self.driver.get(self.url)
 
-    def element_is_present(self, locator, time=20):
+    def element_is_present(self, locator, time=5):
         wait = WebDriverWait(self.driver, time)
         return wait.until(EC.presence_of_element_located(locator),
                           message=f"Can't find element by locator {locator}")
+
+    def execute_script(self, value):
+        return self.driver.execute_script(value)
 
     def type_text(self, locator, text):
         element = self.element_is_present(locator)
@@ -22,15 +26,13 @@ class BasePage:
         element.clear()
         element.send_keys(text)
 
+    def click_element(self, locator):
+        self.element_is_present(locator).click()
+
+    def click_element_with_action(self, locator):
+        element = self.element_is_present(locator)
+        actions = ActionChains(self.driver)
+        return actions.click(element).perform()
+
     def find_elements(self, by, value):
         return self.find_elements(by, value)
-
-    def page_title(self):
-        return self.driver.title
-
-    def page_reload(self):
-        self.driver.refresh()
-
-    def execute_script(self, value):
-        return self.driver.execute_script(value)
-
