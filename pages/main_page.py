349e5f86628a1930d8b6759760e006e1e5f378ff
bonddd <bonddd@gmail.com>
2023-05-10 00:55:20 +0300

    1 file modified main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -17,6 +17,33 @@ class MainPage(BasePage):
         self.element_is_present(MainPageLocators.GEO_HOME)
         self.element_is_present(MainPageLocators.ALICE_ICON)
 
+    def click_menu_burger(self):
+        self.click_element_with_action(MainPageLocators.BURGER_BUTTON)
+
+    def switch_to_frame_menu(self):
+        self.switch_to_frame(*MainPageLocators.FRAME_MENU)
+
+    def elements_are_present_after_click_menu(self):
+        self.element_is_present(MainPageLocators.ACCOUNT_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.CLOSE_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.AUTH_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.MAIL_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.WRITE_MAIL_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.FAVORITES_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.GET_PLUS_BUTTON)
+        self.element_is_present(MainPageLocators.ACCOUNT_MANAGEMENT_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.MY_FEEDBACK_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.SKIN_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.SETTINGS_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.SUPPORT_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.LEGAL_RULES_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.CONFIDENTIAL_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.BLOG_MENU_BUTTON)
+        self.element_is_present(MainPageLocators.ABOUT_COMPANY_MENU_BUTTON)
+
+    def switch_to_main(self):
+        self.switch_to_default_content()
+
     def type_search_request(self):
         self.type_text(MainPageLocators.SEARCH_INPUT, "почта")
 
@@ -31,4 +58,3 @@ class MainPage(BasePage):
         self.element_is_present(MainPageLocators.ALL_SERVICES_BUTTON)
         self.element_is_present(MainPageLocators.CLEAR_BUTTON)
         self.element_is_present(MainPageLocators.SEARCH_BUTTON)
-
